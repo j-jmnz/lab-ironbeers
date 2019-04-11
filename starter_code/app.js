e5092e5691ecee832769354746747e306146a02f
unknown <J@LAPTOP-0NBMDIT9.law.berkeley.edu>
2019-04-11 16:04:14 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,31 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+    punkAPI
+        .getBeers()
+        .then(beers => {
+            res.render('beers', { beers: beers.slice(0, 6) });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
 
+app.get('/randombeer', (req, res, next) => {
+    punkAPI
+        .getRandom()
+        .then(beer => {
+          console.log(beer);
+          res.render('randombeer', { beer });
+        })
+        .catch();
+});
 
 app.listen(3000);
